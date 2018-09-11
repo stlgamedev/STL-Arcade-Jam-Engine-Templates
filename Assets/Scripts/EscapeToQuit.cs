4d65ecf94acb9f462963fa29ebe7ba9259708031
Wes Ehrlichman <wes@superwes.com>
2018-09-11 13:15:59 -0500

    Press Escape to quit - Wes

---
+++ Assets/Scripts/EscapeToQuit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeToQuit : MonoBehaviour {
+    
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject); //this makes the GameObject go through all the scenes. That means, this will work in every scene in the game.
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            Application.Quit();
+            Debug.Log("Application has been quit!");
+        }
+    }
+}
